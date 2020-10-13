2d8a608e521d9b41855e8050236e2394ae9d3767
Freeliquid <freeliquid@freeliquid.info>
2020-10-14 01:46:49 +0300

    oracle - fix tests identations

--- src/oracles.t.sol
+++ src/oracles.t.sol
@@ -14,23 +14,23 @@ import "./oracles.sol";
 
 contract AggregatorV3Stub {
 
-	function decimals() public pure returns (uint8) { return 18; }
-	function description() public pure returns (string memory) { return "TEST"; }
-	function version() public pure returns (uint256) { return 1; }
+  function decimals() public pure returns (uint8) { return 18; }
+  function description() public pure returns (string memory) { return "TEST"; }
+  function version() public pure returns (uint256) { return 1; }
 
-	int256 public answer = 10**18;
+  int256 public answer = 10**18;
 
-	function setAnswer(int256 answer_) public {
-		answer = answer_;
-	}
+  function setAnswer(int256 answer_) public {
+    answer = answer_;
+  }
 
-	function latestRoundData()
-		external
-		view
-		returns (uint80, int256, uint256, uint256, uint80)
-	{
-		return (1, answer, 1602594020, 1602594020, 1);
-	}
+  function latestRoundData()
+    external
+    view
+    returns (uint80, int256, uint256, uint256, uint80)
+  {
+    return (1, answer, 1602594020, 1602594020, 1);
+  }
 }
 
 
@@ -42,23 +42,23 @@ contract UniswapToken is DSToken("UNIv2stub") {
     DSToken t0;
     DSToken t1;
 
-		function setupTokens(DSToken t0_, DSToken t1_) public {
-			t0 = t0_;
-			t1 = t1_;
-		}
+    function setupTokens(DSToken t0_, DSToken t1_) public {
+      t0 = t0_;
+      t1 = t1_;
+    }
 
     function token0() public view returns (address) {
-    	return address(t0);
+      return address(t0);
     }
 
     function token1() public view returns (address) {
-    	return address(t1);
+      return address(t1);
     }
 
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
-    	reserve0 = uint112(t0.balanceOf(address(this)));
-    	reserve1 = uint112(t1.balanceOf(address(this)));
-    	blockTimestampLast = 1;
+      reserve0 = uint112(t0.balanceOf(address(this)));
+      reserve1 = uint112(t1.balanceOf(address(this)));
+      blockTimestampLast = 1;
     }
 
 }
@@ -66,7 +66,7 @@ contract UniswapToken is DSToken("UNIv2stub") {
 
 contract Token is DSToken {
     constructor(bytes32 symbol_, uint d) DSToken (symbol_) public {
-    	decimals = d;
+      decimals = d;
     }
 }
 
@@ -87,74 +87,74 @@ contract OracleTest is DSTest {
     priceETHUSDT = new AggregatorV3Stub();
     priceUSDETH = new AggregatorV3Stub();
 
-		t0 = new Token("USDC", 6);
-		t1 = new Token("USDT", 6);
+    t0 = new Token("USDC", 6);
+    t1 = new Token("USDT", 6);
 
     uni = new UniswapToken(18);
     uni.setupTokens(t0, t1);
 
     oracle = new UniswapAdapterPriceOracle_USDT_USDC();
     oracle.setup(address(priceETHUSDT), address(priceUSDETH), address(uni));
-	}
+  }
 
-	function mintToUni(uint amnt0, uint amnt1) public {
-		(uint112 reserve0b, uint112 reserve1b,) = uni.getReserves();
-		t0.mint(address(uni), amnt0);
-		t1.mint(address(uni), amnt1);
-		(uint112 reserve0, uint112 reserve1,) = uni.getReserves();
+  function mintToUni(uint amnt0, uint amnt1) public {
+    (uint112 reserve0b, uint112 reserve1b,) = uni.getReserves();
+    t0.mint(address(uni), amnt0);
+    t1.mint(address(uni), amnt1);
+    (uint112 reserve0, uint112 reserve1,) = uni.getReserves();
 
-		assertEq(reserve0-reserve0b, amnt0);
-		assertEq(reserve1-reserve1b, amnt1);
-	}
+    assertEq(reserve0-reserve0b, amnt0);
+    assertEq(reserve1-reserve1b, amnt1);
+  }
 
-	function mintToUni(uint amnt) public {
-		mintToUni(amnt, amnt);
-	}
+  function mintToUni(uint amnt) public {
+    mintToUni(amnt, amnt);
+  }
 
-	function readOracle() public returns (uint) {
-		(bytes32 val, bool has) = oracle.peek();
-		assertEq(uint(has?1:0), uint(1));
-		return uint(val);
-	}
+  function readOracle() public returns (uint) {
+    (bytes32 val, bool has) = oracle.peek();
+    assertEq(uint(has?1:0), uint(1));
+    return uint(val);
+  }
 
-	function test1() public {
-		uni.mint(address(this), 1000*(10**18));
-		mintToUni(1000*(10**6));
-		uint val = readOracle();
-		assertEq(uni.totalSupply(), 1000*(10**18));
-		assertEq(t0.totalSupply(), 1000*(10**6));
-		assertEq(t1.totalSupply(), 1000*(10**6));
-		assertEq(val, uint(2 * 10**18));
-	}
+  function test1() public {
+    uni.mint(address(this), 1000*(10**18));
+    mintToUni(1000*(10**6));
+    uint val = readOracle();
+    assertEq(uni.totalSupply(), 1000*(10**18));
+    assertEq(t0.totalSupply(), 1000*(10**6));
+    assertEq(t1.totalSupply(), 1000*(10**6));
+    assertEq(val, uint(2 * 10**18));
+  }
 
 
-	function test2() public {
-		uni.mint(address(this), 1000*(10**18));
-		mintToUni(1000*(10**6));
-
-    priceETHUSDT.setAnswer(2500000000000000);
-    priceUSDETH.setAnswer(200000000000000000000);
-
-		uint val = readOracle();
-		assertEq(uni.totalSupply(), 1000*(10**18));
-		assertEq(t0.totalSupply(), 1000*(10**6));
-		assertEq(t1.totalSupply(), 1000*(10**6));
-		assertEq(val, uint(15 * 10**17));
-	}
-
-	function test3() public {
-		uni.mint(address(this), 1000*(10**18));
-		mintToUni(1000*(10**6), 2000*(10**6));
+  function test2() public {
+    uni.mint(address(this), 1000*(10**18));
+    mintToUni(1000*(10**6));
 
     priceETHUSDT.setAnswer(2500000000000000);
     priceUSDETH.setAnswer(200000000000000000000);
 
-		uint val = readOracle();
-		assertEq(uni.totalSupply(), 1000*(10**18));
-		assertEq(t0.totalSupply(), 1000*(10**6));
-		assertEq(t1.totalSupply(), 2000*(10**6));
-		assertEq(val, uint(2 * 10**18));
-	}
+    uint val = readOracle();
+    assertEq(uni.totalSupply(), 1000*(10**18));
+    assertEq(t0.totalSupply(), 1000*(10**6));
+    assertEq(t1.totalSupply(), 1000*(10**6));
+    assertEq(val, uint(15 * 10**17));
+  }
+
+  function test3() public {
+    uni.mint(address(this), 1000*(10**18));
+    mintToUni(1000*(10**6), 2000*(10**6));
+
+    priceETHUSDT.setAnswer(2500000000000000);
+    priceUSDETH.setAnswer(200000000000000000000);
+
+    uint val = readOracle();
+    assertEq(uni.totalSupply(), 1000*(10**18));
+    assertEq(t0.totalSupply(), 1000*(10**6));
+    assertEq(t1.totalSupply(), 2000*(10**6));
+    assertEq(val, uint(2 * 10**18));
+  }
 
 }
 
